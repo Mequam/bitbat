9a85400ca97db787098a9db58989ac1eae1ee4f2
XGit3000 <sebastian.carrier@gmail.com>
2019-08-20 05:27:18 -0400

    made a barely functional TicTacToe board of arbitrary length.
    it works, but there's some bugs to work out

--- gui/gui.py
+++ gui/gui.py
@@ -1,16 +1,52 @@
 import tkinter as tk
+
+
 class Default:
-	
+
 	def __init__(self, master):
 		frame = master
-		H = int(input("height: "))
-		W = int(input("width: "))		
+		self.H = int(input("height: "))
+		self.W = int(input("width: "))
+		boardvars = []
+		self.playorder = 0
+		self.TICTACGRID = []
+		self.TicVar = tk.IntVar()
+		self.TicVar.set(0)
+		# TicTac = tk.Radiobutton(frame, variable=TicVar)
+		# TicTac.grid(row=0, column=0)
+		self.buttonFrame = tk.Frame(frame)
+		self.buttonFrame.grid(row=0)
+		self.GameFrame = tk.Frame(frame)
+		self.GameFrame.grid(row=1)
+		self.turnButton = tk.Button(self.buttonFrame, text="End Turn", command=self.EndTurn).pack()
+		self.booleanButtontest = tk.BooleanVar
+		for i in range(0, self.H*self.W):
+		# 	boardvars += [tk.BooleanVar()]
+		# 	boardvars[i].set(False)
+		# 	#TICTACGRID += [tk.Radiobutton(frame, variable=boardvars[i], value=i).grid(row=divmod(i,H)[1],column=divmod(i,H)[0])]
+			self.TICTACGRID += [tk.Radiobutton(self.GameFrame, variable=self.TicVar, value=i)]
+			self.TICTACGRID[i].grid(row=divmod(i, self.W)[0], column=divmod(i, self.W)[1])
 
-		
-		for i in range(0, H*W):
-			tk.Checkbutton(frame).grid(row=divmod(i,H)[1],column=divmod(i,H)[0])
-			print(i, "\n", divmod(i,H)[1], divmod(i,H)[0])
-		
+			print(i, "\n", divmod(i,self.H)[1], divmod(i,self.H)[0])
+	def EndTurn(self):
+		playmove = self.TicVar.get()
+		print(playmove)
+		neworder = 0
+		if self.playorder is 0:
+			print(self.TICTACGRID[playmove])
+			self.TICTACGRID[playmove] = tk.Label(self.GameFrame, text="X")
+			self.TICTACGRID[playmove].grid(row=divmod(playmove, self.W)[0], column=divmod(playmove, self.W)[1])
+			print(self.TICTACGRID[playmove])
+			neworder = 1
+		if self.playorder is 1:
+			print(self.TICTACGRID[playmove])
+			self.TICTACGRID[playmove] = tk.Label(self.GameFrame, text="O")
+			self.TICTACGRID[playmove].grid(row=divmod(playmove, self.W)[0], column=divmod(playmove, self.W)[1])
+			print(self.TICTACGRID[playmove])
+			neworder = 0
+		self.playorder = neworder
+		print(self.TICTACGRID)
+
 root = tk.Tk()
 b = Default(root)
 root.mainloop()
